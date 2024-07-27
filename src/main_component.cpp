7dceaec2a894424e35c4b046475dcbdcdf098a3b
James Ferrarelli <ferrarellijames@gmail.com>
2024-07-27 18:54:32 -0400

    Clean up staff view
    Add utility function to draw a line with or without stroke.
    Remember that the Graphics::drawLine function can accept juce::Point<>'s.
    Even out the margins, and extract out the number of horizontal lines for future variability in guitar string counts.

--- src/main_component.cpp
+++ src/main_component.cpp
@@ -1,6 +1,6 @@
 #include <main_component.h>
 
-MainComponent::MainComponent() {
+MainComponent::MainComponent() : string_count(6) {
     setSize(1200, 900);
 
 	// TODO: add menu items to menuModel
@@ -10,24 +10,32 @@ MainComponent::MainComponent() {
 	// TODO: style menubar
     addAndMakeVisible(menuBar);
 
-	lines.reserve(100);
-    populateLines(lines);
+    populateLines();
 }
 
-void MainComponent::populateLines(std::vector<juce::Line<float>> &line_list) {
+void MainComponent::appendWithStroke(juce::Line<float> a_line, float a_stroke) {
+	lines.push_back({ a_line, a_stroke });
+}
+
+void MainComponent::populateLines() {
 	const float margin = 48;
-	juce::Point<float> staff_dim = { getWidth() - margin, 150 };
 
-	juce::Rectangle<float> staff_border = { margin, margin, staff_dim.getX(), staff_dim.getY() };
+	juce::Rectangle<float> staff_border = { margin, margin, getWidth() - 2 * margin, 150 };
 
-	line_list.push_back({ staff_border.getX(), staff_border.getY(), staff_border.getWidth(), staff_border.getY() });
-	line_list.push_back({ staff_border.getX(), staff_border.getY(), staff_border.getX(), staff_border.getHeight() });
-	line_list.push_back({ staff_border.getX(), staff_border.getHeight(), staff_border.getWidth(), staff_border.getHeight() });
-	line_list.push_back({ staff_border.getWidth(), staff_border.getY(), staff_border.getWidth(), staff_border.getHeight() });
+	lines.reserve(100);
+	// bad potentially - this performs two copies and two destructions, but runs on load so eh?
+	appendWithStroke(juce::Line<float>{ staff_border.getTopLeft(), staff_border.getTopRight() });
+	appendWithStroke(juce::Line<float>{ staff_border.getTopLeft(), staff_border.getBottomLeft() });
+	appendWithStroke(juce::Line<float>{ staff_border.getBottomLeft(), staff_border.getBottomRight() });
+	appendWithStroke(juce::Line<float>{ staff_border.getTopRight(), staff_border.getBottomRight() });
 
-	for (int i = 1; i < 5; ++i) {
-		float sub_line_y = (staff_border.getHeight() / 7) * i + staff_border.getY();
-		line_list.push_back({ staff_border.getX(), sub_line_y, staff_border.getWidth(), sub_line_y });
+	// big stroke on beginning of first measure in a chart, and one at the end
+	appendWithStroke(juce::Line<float>{ staff_border.getTopLeft(), staff_border.getBottomLeft() }, 15.f);
+	appendWithStroke(juce::Line<float>{ staff_border.getTopRight(), staff_border.getBottomRight() }, 15.f);
+
+	for (int i = 1; i < string_count; ++i) {
+		float sub_line_y = (staff_border.getHeight() / string_count) * i + staff_border.getY();
+		appendWithStroke(juce::Line<float>{ staff_border.getX(), sub_line_y, staff_border.getRight(), sub_line_y });
 	}
 }
 
@@ -35,13 +43,10 @@ void MainComponent::paint(juce::Graphics& g) {
 	g.fillAll(juce::Colours::black);
 	g.setColour(juce::Colours::white);
 
-	for (auto &line : lines) {
-		g.drawLine(line, 0.5f);
+	for (auto &line_pair : lines) {
+		auto [line, stroke] = line_pair;
+		g.drawLine(line, stroke);
 	}
-
-	// big assumption here
-	const juce::Line<float> begin_line = lines.at(0);
-	g.drawLine({ begin_line.getStart().getX(), begin_line.getStart().getY(), begin_line.getStart().getX(), begin_line.getEnd().getY() }, 5.f);
 }
 
 void MainComponent::resized() {
--- src/main_component.h
+++ src/main_component.h
@@ -10,10 +10,13 @@ public:
     void resized() override;
 
 private:
-	void populateLines(std::vector<juce::Line<float>> &line_list);
+	void populateLines();
+	void appendWithStroke(juce::Line<float> a_line, float stroke = 0.5f);
 
-	std::vector<juce::Line<float>> lines;
+	std::vector<std::pair<juce::Line<float>, float>> lines;
 	juce::MenuBarComponent menuBar;
+
+	int string_count;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
